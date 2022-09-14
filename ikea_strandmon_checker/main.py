0af5e9afa3be096d18c9ef6b0822e53b61b29659
Marcin Marczyk <marcin.marczyk@ocado.com>
2022-09-14 12:05:01 +0200

    ikea_strandmon_checker:1.0.10

--- ikea_strandmon_checker/main.py
+++ ikea_strandmon_checker/main.py
@@ -5,15 +5,21 @@ from bs4 import BeautifulSoup
 import json
 import os
 import logging
+import logging.handlers
 
-log = logging.getLogger("ikea_strandmon_checker")
+handler = logging.handlers.WatchedFileHandler(
+    os.environ.get("LOGFILE", "/var/log/ikea_strandmon_checker.log"))
+formatter = logging.Formatter(logging.BASIC_FORMAT)
+handler.setFormatter(formatter)
+root = logging.getLogger()
+root.addHandler(handler)
 
 token = os.getenv("SUPERVISOR_TOKEN")
 
 s = requests.Session()
 
 def send_entity_state(price):
-    log.info("TOKEN: " + str(token))
+    logging.info("TOKEN: " + str(token))
     ha_headers = {
         "Authorization": "Bearer {}".format(token),
         "Content-Type": "application/json"
@@ -22,21 +28,21 @@ def send_entity_state(price):
         "state": price
     }
     ha_response = requests.post("http://supervisor/core/api/states/sensor.ikea_strandmon", json=ha_payload, headers=ha_headers)
-    log.info(ha_response.status_code)
-    log.info(ha_response.text)
+    logging.info(ha_response.status_code)
+    logging.info(ha_response.text)
 
 def get_price():
     global price
     price_page = s.get('https://www.ikea.com/pl/pl/products/844/80359844-compact-fragment.html')
     p = BeautifulSoup(price_page.text, 'html.parser')
     price_str = p.find_all(attrs={'class': 'pip-price__integer'})[0].text
-    log.info(price_str)
+    logging.info(price_str)
     price = float(price_str)
     return price
 
 
 if __name__ == '__main__':
-    log.info("Addon started")
+    logging.info("Addon started")
     while True:
         price = get_price()
         send_entity_state(price)
