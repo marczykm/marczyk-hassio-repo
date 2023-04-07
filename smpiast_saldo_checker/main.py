99f68a17d9f5961ec405a80c8b6405eea9ed364c
Marcin Marczyk <marcin.marczyk@ocado.com>
2023-04-07 09:14:52 +0200

    smpiast_saldo_checker:1.0.4

--- smpiast_saldo_checker/main.py
+++ smpiast_saldo_checker/main.py
@@ -10,6 +10,8 @@ print("token: {}".format(token))
 
 s = requests.Session()
 options = {}
+salt = ''
+instance = ''
 
 def send_entity_state(saldo):
     ha_headers = {
@@ -27,12 +29,13 @@ def send_entity_state(saldo):
 def load_options():
     global options
     f = open("/data/options.json", "r")
+    # f = open("data/options.json", "r")
     options = json.loads(f.read())
     f.close()
 
 
 def login_():
-    global saldo_url
+    global saldo_url, salt, instance
     index = s.get('https://ebok.smpiast.com.pl/ords/f?p=100:102::::RP,102::')
     parsed_html = BeautifulSoup(index.text, 'html.parser')
     salt = parsed_html.find(id='pSalt')['value']
@@ -68,15 +71,40 @@ def login_():
 
 
 def get_saldo():
-    global saldo
+    global saldo, salt, instance
     saldo_page = s.get('https://ebok.smpiast.com.pl/ords/' + saldo_url)
+    # time.sleep(5)
     p = BeautifulSoup(saldo_page.text, 'html.parser')
-    username = p.find_all(attrs={'class': 't-Button-label'})[0].text
-    print('https://ebok.smpiast.com.pl/ords/' + saldo_url)
-    saldo = float(p.find_all(attrs={'id': 'N_SALDO'})[0].text.split(': ')[1].split(' zł')[0]
-                  .replace(' ', '').replace(',', '.'))
-    print(username)
-    print(saldo)
+    js = p.find_all('script')[12].get_text()
+    i = js.find('R_SALDO_WEDLUG_MIEJSC_24') + 27
+    plugin = js[i:i+132]
+    plugin = plugin.replace('\\', "/")
+    plugin = plugin.replace('u002F', "")
+    print('plugin: ' + plugin)
+    p_json = {
+        "salt": salt
+    }
+    payload = {
+        'p_flow_id': 100,
+        'p_flow_step_id': 24,
+        'p_instance': instance,
+        'p_debug': '',
+        'p_request': 'PLUGIN='+plugin,
+        'p_widget_action': 'reset',
+        'x01': 31951121851046320,
+        'p_json': json.dumps(p_json)
+    }
+    index = s.post('https://ebok.smpiast.com.pl/ords/wwv_flow.ajax', data=payload)
+    p = BeautifulSoup(index.text, 'html.parser')
+    saldo_text = p.find_all(attrs={'class': 't-Card-desc'})[0].get_text()
+    i = saldo_text.find(':')+2
+    saldo_text = saldo_text[i:-1]
+    i = saldo_text.find('zł')
+    saldo_text = saldo_text[0:i]
+    saldo_text = saldo_text.replace(',', '.')
+    print(saldo_text)
+    saldo = float(saldo_text)
+    
     return saldo
 
 
@@ -86,5 +114,5 @@ if __name__ == '__main__':
         saldo_url = login_()
         saldo = get_saldo()
         send_entity_state(saldo)
-        time.sleep(60*5)
+        time.sleep(60*60)
 
